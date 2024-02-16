f243b5e96d96aba4ae15440dc86b9272af7cb237
Andrey Kochyk <kochyk1999@gmail.com>
2024-02-16 18:42:38 +0300

    First commit

---
+++ src/main/java/by/kochyk/ReputonTestTask/ReputonTestTaskApplication.java
@@ -0,0 +1,13 @@
+package by.kochyk.ReputonTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputonTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputonTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kochyk/ReputonTestTask/ReputonTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kochyk.ReputonTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputonTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
